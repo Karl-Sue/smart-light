d36ca1aefbf00fc15c9e48bef7f79e29b4fc20d9
Karl_Sue <tuankhanh17032003@gmail.com>
2026-03-12 18:12:58 +0800

    bug: fixed version of nodejs

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/smartlight/', // Adjusted for GitHub pages deployment
+  base: '/smart-light/', // Adjusted to match your exact GitHub repository name
   plugins: [react()],
 })
